1b2e080880bfd78c7baf477f514987516d6e3f7c
Andres Caruncho <nescaruncho@gmail.com>
2024-10-03 14:32:19 +0200

    first commit

---
+++ UD2/Ejemplos/operacionesstring.js
@@ -0,0 +1,20 @@
+var nombre = "Bruce Wayne";
+
+console.log(nombre);
+console.log(typeof(nombre));
+console.log(nombre.toUpperCase());
+console.log(nombre.toLowerCase());
+
+console.log(nombre.length);
+
+console.log(nombre.charAt(0));
+console.log(nombre.charAt(-1));
+
+console.log(nombre.indexOf("u"));
+console.log(nombre.lastIndexOf("ce"));
+console.log(nombre.indexOf("Super"));
+
+console.log(nombre.substring(6));
+console.log(nombre.substring(6,9));
+
+console.log(nombre.replace("e","i"));
---
+++ UD2/Tareas/declaracionvariables.js
@@ -0,0 +1,27 @@
+/* Crear un programa JavaScript que empregue tipos de variables distintas para almacenar os datos persoais típicos dunha persoa. Débense empregar nomes con todas as posibilidades de nomenclatura.
+Garda o resultado como un pdf(o código debe poderse copiar e pegar).
+Engade un comentario co teu nome. Engade os comentarios que consideres necesarios. */
+
+/* Andrés Francisco Caruncho Faci */
+
+//Nombre y apellidos
+var nombre = "Manuel";
+var apellido1 = "González";
+var apellido2 = "Pérez";
+
+//Datos identidad
+var $fechanacimiento = "01-01-1970";
+var $lugardenacimiento = "Padrón";
+var $nacionalidad = "España";
+var $dni = "11111111A";
+
+//Dirección
+var linea1_direccion = "Calle del Paseo";
+var linea2_direccion = null;
+var numero = 1;
+var piso = 2;
+var letra = "C";
+
+//Otros
+var comentarios = "";
+var aprobado = true;
